d40c6aeae1933568fbf8c5489363337fe62d0e5e
Han Nguyen <hanvnguyen94@gmail.com>
2021-04-08 12:43:05 -0400

    add more songs

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
 	// state
 	const [songs, setSongs] = useState(data())
-	const [currentSong, setCurrentSong] = useState(songs[1])
+	const [currentSong, setCurrentSong] = useState(songs[0])
 	const [isPlaying, setIsPlaying] = useState(false)
 	const [songInfo, setSongInfo] = useState({
 		currentTime: 0,
--- src/songsData.js
+++ src/songsData.js
@@ -3,6 +3,16 @@ import { v4 as uuidv4 } from 'uuid'
 function chillHop() {
 	return [
 		{
+			name: 'Lonely Waves',
+			cover:
+				'https://chillhop.com/wp-content/uploads/2021/02/80be32197ca7971424bef5e4d0c6810e1d4334c6-1024x1024.jpg',
+			artist: 'C Y G N',
+			audio: 'https://mp3.chillhop.com/serve.php/?mp3=14980',
+			color: ['##5A3D6B', '##E589F4'],
+			id: uuidv4(),
+			active: true,
+		},
+		{
 			name: 'Beaver Creek',
 			cover:
 				'https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg',
@@ -10,7 +20,7 @@ function chillHop() {
 			audio: 'https://mp3.chillhop.com/serve.php/?mp3=10075',
 			color: ['#205950', '#2ab3bf'],
 			id: uuidv4(),
-			active: true,
+			active: false,
 		},
 		{
 			name: 'Daylight',
@@ -72,36 +82,7 @@ function chillHop() {
 			id: uuidv4(),
 			active: false,
 		},
-		{
-			name: 'Fade',
-			cover:
-				'https://chillhop.com/wp-content/uploads/2020/07/04d5dd66c33d8fc9137fd7b47f3b3c3a8e571761-1024x1024.jpg',
-			artist: 'Knowmadic',
-			audio: 'https://mp3.chillhop.com/serve.php/?mp3=1733',
-			color: ['#B64E65', '#393B61'],
-			id: uuidv4(),
-			active: false,
-		},
-		{
-			name: 'Fade',
-			cover:
-				'https://chillhop.com/wp-content/uploads/2020/07/04d5dd66c33d8fc9137fd7b47f3b3c3a8e571761-1024x1024.jpg',
-			artist: 'Knowmadic',
-			audio: 'https://mp3.chillhop.com/serve.php/?mp3=1733',
-			color: ['#B64E65', '#393B61'],
-			id: uuidv4(),
-			active: false,
-		},
-		{
-			name: 'Fade',
-			cover:
-				'https://chillhop.com/wp-content/uploads/2020/07/04d5dd66c33d8fc9137fd7b47f3b3c3a8e571761-1024x1024.jpg',
-			artist: 'Knowmadic',
-			audio: 'https://mp3.chillhop.com/serve.php/?mp3=1733',
-			color: ['#B64E65', '#393B61'],
-			id: uuidv4(),
-			active: false,
-		},
+
 		//ADD MORE HERE
 	]
 }
